085850eab3e13aacb568e51097600fd3373b15e9
Jeferson Villacis <jeeh86@hotmail.com>
2026-03-24 15:53:01 -0500

    refactor: simplify the Kafka configuration logic

--- src/main/java/com/villacis/webflux/infrastructure/input/adapter/kafka/config/KafkaConfig.java
+++ src/main/java/com/villacis/webflux/infrastructure/input/adapter/kafka/config/KafkaConfig.java
@@ -1,8 +1,6 @@
 package com.villacis.webflux.infrastructure.input.adapter.kafka.config;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -10,7 +8,6 @@ import org.springframework.kafka.core.reactive.ReactiveKafkaConsumerTemplate;
 import reactor.kafka.receiver.ReceiverOptions;
 
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -18,32 +15,24 @@ import java.util.Map;
  * @version 1.0
  * @since 09/08/2025 Copyright 2025
  */
-@Slf4j
 @EnableKafka
 @Configuration
-@RequiredArgsConstructor
 public class KafkaConfig {
-  private final KafkaPropertiesConfig kafkaProperties;
+  private final KafkaProperties kafkaProperties;
+  private final KafkaPropertiesConfig kafkaPropertiesConfig;
+
+  public KafkaConfig(KafkaProperties kafkaProperties, KafkaPropertiesConfig kafkaPropertiesConfig) {
+    this.kafkaProperties = kafkaProperties;
+    this.kafkaPropertiesConfig = kafkaPropertiesConfig;
+  }
 
   @Bean
   public ReactiveKafkaConsumerTemplate<String, String> reactiveKafkaConsumerTemplate() {
-    Map<String, Object> consumerProperties = new HashMap<>();
-    consumerProperties.put(
-        ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
-    consumerProperties.put(
-        ConsumerConfig.GROUP_ID_CONFIG, kafkaProperties.getTopic().getName() + "-group");
-    consumerProperties.put(
-        ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,
-        kafkaProperties.getConsumer().getAutoOffsetReset());
-    consumerProperties.put(
-        ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
-        kafkaProperties.getConsumer().getKeyDeserializer());
-    consumerProperties.put(
-        ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
-        kafkaProperties.getConsumer().getValueDeserializer());
+    Map<String, Object> consumerProperties = kafkaProperties.buildConsumerProperties();
+
     ReceiverOptions<String, String> receiverOptions =
         ReceiverOptions.<String, String>create(consumerProperties)
-            .subscription(Collections.singleton(kafkaProperties.getTopic().getName()));
+            .subscription(Collections.singleton(kafkaPropertiesConfig.getTopic().getName()));
     return new ReactiveKafkaConsumerTemplate<>(receiverOptions);
   }
 }
